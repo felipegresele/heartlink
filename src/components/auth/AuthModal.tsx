ee96e696d2a01afbb01697ef7666171ec4af109c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 10:40:01 -0300

    cadastro pronto

---
+++ src/components/auth/AuthModal.tsx
@@ -0,0 +1,187 @@
+import { Controller, useForm } from "react-hook-form";
+import { IoClose } from "react-icons/io5";
+
+// Definindo o tipo de dados do formulário
+type AuthFormProps = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  displayName: string;
+};
+
+// Definindo o tipo da propriedade para o modal
+interface AuthModalProps {
+  fecharModal: () => void; // Função para fechar o modal
+}
+
+export function AuthModal({ fecharModal }: AuthModalProps) {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg w-full sm:w-100">
+        {/* Fechar Modal */}
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-xl font-bold text-white">Heartlink</h1>
+          <button onClick={fecharModal}>
+            <IoClose size={20} color="white" />
+          </button>
+        </div>
+
+        {/* Formulário de autenticação */}
+        <AuthForm fecharModal={fecharModal} />
+      </div>
+    </div>
+  );
+}
+
+export function AuthForm({ fecharModal }: AuthModalProps) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    getValues
+  } = useForm<AuthFormProps>({
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      displayName: "",
+    },
+  });
+
+  const onSubmit = (data: AuthFormProps) => {
+    console.log(data);
+    fecharModal()
+    // Aqui você pode adicionar lógica de envio de dados
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="text-white">
+      <h2 className="text-lg font-semibold mb-4 text-white">Crie sua conta</h2>
+
+      {/* Nome Completo */}
+      <Controller
+        name="displayName"
+        control={control}
+        rules={{
+          required: { value: true, message: "Nome é obrigatório" },
+          maxLength: { value: 30, message: "O nome deve ter no máximo 30 caracteres" },
+          minLength: { value: 3, message: "O nome deve ter no mínimo 3 caracteres" },
+        }}
+        render={({ field }) => (
+          <>
+            <label className="block text-sm font-medium mb-1 text-white">Nome Completo:</label>
+            <input
+              {...field}
+              placeholder="Digite seu nome completo"
+              className="w-full p-2 border border-gray-600 rounded-md mb-2 bg-gray-700 text-white focus:outline-none focus:border-red-500"
+            />
+            {errors.displayName && (
+              <p className="text-xs text-red-500">{errors.displayName.message}</p>
+            )}
+          </>
+        )}
+      />
+
+      {/* Email */}
+      <Controller
+        name="email"
+        control={control}
+        rules={{
+          required: { value: true, message: "Email é obrigatório" },
+          validate: async (value) => {
+            try {
+                const resp = await fetch(`https://emailvalidation.abstractapi.com/v1/?api_key=f789f509a13643a3ae9ec34a6fb22d6c&email=${value}`)
+                const data = await resp.json(); //transformando a resp em json no data
+
+                if (!data.is_valid_format?.value) {
+                    return "Formato de e-mail inválido";
+                }
+
+                if (!data.deliverability || data.deliverability === "UNDELIVERABLE") {
+                    return "Esse email é inválido"
+                }
+
+                return true
+            } catch (error) {
+                console.error(error)
+                return "Erro ao validar -mail"
+            }
+          }
+        }}
+        render={({ field }) => (
+          <>
+            <label className="block text-sm font-medium mb-1 text-white">Email:</label>
+            <input
+              {...field}
+              placeholder="Digite seu email"
+              className="w-full p-2 border border-gray-600 rounded-md mb-2 bg-gray-700 text-white focus:outline-none focus:border-red-500"
+            />
+            {errors.email && (
+              <p className="text-xs text-red-500">{errors.email.message}</p>
+            )}
+          </>
+        )}
+      />
+
+      {/* Senha */}
+      <Controller
+        name="password"
+        control={control}
+        rules={{
+          required: { value: true, message: "Senha é obrigatória" },
+        }}
+        render={({ field }) => (
+          <>
+            <label className="block text-sm font-medium mb-1 text-white">Senha:</label>
+            <input
+              {...field}
+              placeholder="Digite sua senha"
+              type="password"
+              className="w-full p-2 border border-gray-600 rounded-md mb-2 bg-gray-700 text-white focus:outline-none focus:border-red-500"
+            />
+            {errors.password && (
+              <p className="text-xs text-red-500">{errors.password.message}</p>
+            )}
+          </>
+        )}
+      />
+
+      <Controller
+        name="confirmPassword"
+        control={control}
+        rules={{
+          required: { value: true, message: "Confirme a senha por favor" },
+          validate: (value) => {
+            const senha = getValues("password")
+
+            if(value !== senha) {
+                return "As senhas não coincidem"
+            }
+            return true
+          }
+        }}
+        render={({ field }) => (
+          <>
+            <label className="block text-sm font-medium mb-1 text-white">Confirmar Senha:</label>
+            <input
+              {...field}
+              placeholder="Confirme sua senha"
+              type="password"
+              className="w-full p-2 border border-gray-600 rounded-md mb-2 bg-gray-700 text-white focus:outline-none focus:border-red-500"
+            />
+            {errors.confirmPassword && (
+              <p className="text-xs text-red-500">{errors.confirmPassword.message}</p>
+            )}
+          </>
+        )}
+      />
+
+      <button
+        type="submit"
+        className="w-full mt-4 bg-red-700 font-bold text-white py-2 rounded-md hover:bg-red-500 focus:outline-none"
+      >
+        Criar Conta
+      </button>
+    </form>
+  );
+}
--- src/components/header/route.tsx
+++ src/components/header/route.tsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 import img from "../../assets/img.jpg";
-import { IoPersonOutline} from "react-icons/io5";
+import { useState } from "react";
+import { AuthModal } from "../auth/AuthModal"; // Certifique-se de que o caminho está correto
 
 export function Header() {
+  const [exibirModal, setExibirModal] = useState(false);
+
+  const abrirModal = () => {
+    setExibirModal(true);
+  };
+
+  const fecharModal = () => {
+    setExibirModal(false);
+  };
+
   return (
     <div className="bg-black p-6 justify-between flex items-center">
       <div className="flex gap-2 justify-center items-center">
@@ -19,10 +30,22 @@ export function Header() {
         <Link to="/help">F.A.Q</Link>
       </div>
       <div className="text-white">
-        <Link to="conta" className="flex items-center text-[15px] font-bold mr-5">
-        <IoPersonOutline size={20} className="mr-3" />
-        Minha conta
-        </Link>
+        <div className="flex gap-4">
+          <Link
+            to="/login" // Rota de login
+            className="bg-black border-1 border-red-500 text-white py-2 px-4 rounded-md hover:bg-gray-600 font-bold"
+          >
+            Entrar
+          </Link>
+          <button
+            className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 font-bold w-30 h-10"
+            onClick={abrirModal} // Abre o modal de Cadastro
+          >
+            Cadastrar
+          </button>
+        </div>
+
+        {exibirModal && <AuthModal fecharModal={fecharModal} />}
       </div>
     </div>
   );

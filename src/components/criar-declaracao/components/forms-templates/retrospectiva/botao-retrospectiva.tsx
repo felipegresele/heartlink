75bb84de27953a54356a81743d8a5c3c7cbb9d75
felipegresele <felipehgresele@gmail.com>
2026-04-18 14:26:17 -0300

    fix: boato retrospectiva ajustado e melhorado

--- src/components/criar-declaracao/components/forms-templates/retrospectiva/botao-retrospectiva.tsx
+++ src/components/criar-declaracao/components/forms-templates/retrospectiva/botao-retrospectiva.tsx
@@ -1,17 +1,26 @@
 import img from "../../../../../img/bg-retrospectiva.png";
 
 export function RetrospectiveBtn({ isVisible }: { isVisible: () => void }) {
-
   return (
-    <div>
-        <div className="w-100 h-110">
-          <img src={img} className="w-80 h-full object-cover rounded-md" />
-          <div className="absolute inset-0 flex items-end justify-center pb-6">
-            <button onClick={isVisible} className="bg-green-500 font-bold text-black w-35 h-12 mb-30 rounded-md cursor-pointer">
-              Acessar
-            </button>
-          </div>
-        </div>
+    <div className="flex flex-col justify-center items-center mt-4 h-150">
+      
+      <div className="flex justify-center items-center text-center">
+        <img
+          src={img}
+          className="w-70 h-100 object-cover rounded-md"
+        />
+      </div>
+
+      {/* BOTÃO COM BORDA ANIMADA */}
+      <div className="p-[2px] rounded-md bg-gradient-to-r from-gray-500 via-red-500 to-pink-500 animate-border mt-4 mb-20">
+        <button
+          onClick={isVisible}
+          className="bg-black text-white font-bold w-40 h-12 rounded-md cursor-pointer hover:scale-105 transition"
+        >
+          Acessar
+        </button>
+      </div>
+
     </div>
   );
 }

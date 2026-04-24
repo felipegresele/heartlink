cbe3cd435e4b4573e6f9a4ec855364ed583049f4
felipegresele <felipehgresele@gmail.com>
2026-04-23 23:33:32 -0300

    aumentando logo

--- src/components/criar-declaracao/components/tela-presente/template-padrao/page-ready-template-padrao.tsx
+++ src/components/criar-declaracao/components/tela-presente/template-padrao/page-ready-template-padrao.tsx
@@ -822,7 +822,8 @@ export default function PageReady({
           )}
         </AnimatePresence>
 
-        <img src={imgLogo} className="h-30" />
+        {/* 144px */}
+        <img src={imgLogo} className="h-36 w-36" />
 
         <div className="bg-gray-800 rounded-xl p-6 w-full max-w-2xl flex flex-col items-center shadow-xl">
           <div className="bg-white w-[320px] flex flex-col items-center justify-center px-3 pt-3 pb-6 shadow-lg">

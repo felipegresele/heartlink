a0d626a4e3db3144254f0c6b51bf36fd5944dd74
felipegresele <felipehgresele@gmail.com>
2026-04-27 09:18:37 -0300

    ajuste

--- src/components/boasvindas/route.tsx
+++ src/components/boasvindas/route.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { FaHeartCircleExclamation } from "react-icons/fa6";
 import imgCelular from "../../img/hero-section.png";
 import { AvaliacoesDeslizando } from "../avaliacoes/card-avaliacoes";
+import { HiHeart } from "react-icons/hi2";
 
 export function BoasVindas() {
   return (
@@ -18,59 +19,52 @@ export function BoasVindas() {
           className="absolute top-[8%] left-[5%] text-pink-300 text-3xl animate-float"
           style={{ animationDuration: "4s", animationDelay: "0s" }}
         >
-          ♥
+          <HiHeart />
         </span>
-
         <span
           className="absolute top-[15%] right-[8%] text-pink-400 text-xl animate-float"
           style={{ animationDuration: "5s", animationDelay: "0.5s" }}
         >
-          ♥
+          <HiHeart />
         </span>
-
         <span
           className="absolute top-[55%] left-[3%] text-fuchsia-300 text-base animate-float"
           style={{ animationDuration: "3.5s", animationDelay: "1s" }}
         >
-          ♥
+          <HiHeart />
         </span>
-
         <span
           className="absolute bottom-[12%] left-[10%] text-pink-300 text-2xl animate-float"
           style={{ animationDuration: "4.5s", animationDelay: "0.3s" }}
         >
-          ♥
+          <HiHeart />
         </span>
-
         <span
           className="absolute bottom-[20%] right-[6%] text-pink-400 text-4xl animate-float"
           style={{ animationDuration: "6s", animationDelay: "0.8s" }}
         >
-          ♥
+          <HiHeart />
         </span>
-
         <span
           className="absolute top-[38%] right-[4%] text-fuchsia-300 text-sm animate-float"
           style={{ animationDuration: "3s", animationDelay: "1.2s" }}
         >
-          ♥
+          <HiHeart />
         </span>
-
         <span
           className="absolute top-[70%] right-[14%] text-pink-300 text-lg animate-float"
           style={{ animationDuration: "5s", animationDelay: "0.6s" }}
         >
-          ♥
+          <HiHeart />
         </span>
-
         <span
           className="absolute top-[28%] left-[7%] text-fuchsia-400 text-xs animate-float"
           style={{ animationDuration: "4s", animationDelay: "1.5s" }}
         >
-          ♥
+          <HiHeart />
         </span>
       </div>
-
+      
       {/* Conteúdo esquerdo */}
       <div className="relative max-w-lg z-10">
         <h1 className="text-5xl md:text-6xl mt-6 font-bold mb-4 text-gray-900">
@@ -104,16 +98,16 @@ export function BoasVindas() {
 
         {/* Corações ao redor do celular */}
         <span className="absolute top-2 right-6 md:right-16 text-pink-400 text-3xl animate-[float_4s_ease-in-out_infinite] z-20">
-          ♥
+          <HiHeart />
         </span>
         <span className="absolute bottom-4 left-6 md:left-16 text-fuchsia-400 text-2xl animate-[float_5s_ease-in-out_infinite_0.7s] z-20">
-          ♥
+          <HiHeart />
         </span>
         <span className="absolute top-1/3 left-2 md:left-8 text-pink-300 text-lg animate-[float_3.5s_ease-in-out_infinite_1.1s] z-20">
-          ♥
+          <HiHeart />
         </span>
         <span className="absolute bottom-1/3 right-2 md:right-8 text-pink-400 text-xl animate-[float_4.5s_ease-in-out_infinite_0.4s] z-20">
-          ♥
+          <HiHeart />
         </span>
 
         <div className="relative">

8a512bff35f75d37e72795d2fe4ddc5454ca25e3
felipegresele <felipehgresele@gmail.com>
2026-07-08 10:36:30 -0300

    botao ajustado retrospectiva do spotify

--- src/components/criar-declaracao/components/forms-templates/retrospectiva/botao-retrospectiva-spotify.tsx
+++ src/components/criar-declaracao/components/forms-templates/retrospectiva/botao-retrospectiva-spotify.tsx
@@ -13,23 +13,22 @@ export function RetrospectiveSpotifyBtn({
           "linear-gradient(180deg, #111827 0%, #0a0a0a 55%, #000000 100%)",
       }}
     >
-      <div className="w-full max-w-sm px-4 flex justify-center items-center text-center">
-        <button
+      <div
+        role="button"
+        tabIndex={0}
         onClick={isVisible}
-        className="mt-6 bg-[#3EC6F0] text-black font-bold w-40 h-12 rounded-full cursor-pointer hover:scale-105 active:scale-95 transition"
-      ></button>
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === " ") {
+            isVisible();
+          }
+        }}
+        className="w-full rounded-2xl cursor-pointer hover:scale-105 active:scale-95 transition overflow-hidden"
+      >
         <img
           src={imgBanner}
-          className="w-full h-auto object-contain rounded-xl"
+          className="w-250 h-130 object-cover rounded-2xl"
         />
       </div>
-
-      <button
-        onClick={isVisible}
-        className="mt-6 bg-[#3EC6F0] text-black font-bold w-40 h-12 rounded-full cursor-pointer hover:scale-105 active:scale-95 transition"
-      >
-        Acessar
-      </button>
     </div>
   );
 }
